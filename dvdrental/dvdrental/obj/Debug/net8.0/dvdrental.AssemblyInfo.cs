9798d8f8f96ffe662cef2018e8ff85c4718048ea
Sopraj1120 <manchukansopraj@gmail.com>
2024-10-30 12:47:18 +0530

    Add Sql Query

--- dvdrental/dvdrental/obj/Debug/net8.0/dvdrental.AssemblyInfo.cs
+++ dvdrental/dvdrental/obj/Debug/net8.0/dvdrental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dvdrental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667830cd5a8fc476c8005d9078911758029fd1a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd461ff7aea762988cd22b008fc352f210ecd35")]
 [assembly: System.Reflection.AssemblyProductAttribute("dvdrental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dvdrental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
